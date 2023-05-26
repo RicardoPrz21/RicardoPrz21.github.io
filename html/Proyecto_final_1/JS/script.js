1b53d69b376180ebb47ac29fbcc018b4332d315d
Ricardo Daniel Perez Briceño <130397019+RicardoPrz21@users.noreply.github.com>
2023-05-26 12:07:56 -0600

    Add files via upload

---
+++ html/Proyecto_final_1/JS/script.js
@@ -0,0 +1,24 @@
+document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+
+    document.querySelector(".menu").classList.toggle("mostrar_menu");
+    
+}
+
+window.onscroll = function () {
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
+    var elemento1 = document.getElementById("icon_heart");
+    var elemento2 = document.getElementById("icon_fire");
+    elemento1.style.bottom = posicion * 0.1 + "px";
+    elemento2.style.top = posicion * 0.1 + "px";
+}
+
+function cargaPractica(htmlDocument) {
+	document.getElementById("content").innerHTML = '<object width=\"100%\" height=\"100%\" type="text/html" data="' + htmlDocument + '" ></object>';
+}
+
+const btnRedirect = document.getElementById('btnRedirect');
+btnRedirect.addEventListener('click', function() {
+  window.location.href = '/index.html';
+});
