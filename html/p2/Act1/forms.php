87e6cdfbff7f248b3a1e4b59516f5c3cb2cfe922
Ricardo Daniel Perez Briceño <130397019+RicardoPrz21@users.noreply.github.com>
2024-03-09 22:02:25 -0600

    Add files via upload

---
+++ html/p2/Act1/forms.php
@@ -0,0 +1,31 @@
+<?php
+    $nombre = $_POST['nombre'];
+    $apellido_p = $_POST['apellido_p'];
+    $apellido_m = $_POST['apellido_m'];
+    $usuario = $_POST['username'];
+    $contraseña = $_POST['password'];
+    $f_nacimiento = $_POST['dateofbirth'];
+    $pais = $_POST['pais'];
+    $ciudad = $_POST['ciudad'];
+    $calle = $_POST['calle'];
+    $numero_casa = $_POST['numero_casa'];
+    $colonia = $_POST['col'];
+    $email = $_POST['email'];
+    $rfc = $_POST['rfc'];
+    $postal = $_POST['postal'];
+
+    echo "$nombre.<br>"; 
+    echo "$usuario.<br>";
+    echo "$usuario.<br>"; 
+    echo "$contraseña.<br>"; 
+    echo "$f_nacimiento.<br>";  
+    echo "$pais.<br>"; 
+    echo "$ciudad.<br>"; 
+    echo "$calle.<br>"; 
+    echo "$numero_casa.<br>"; 
+    echo "$colonia.<br>"; 
+    echo "$email.<br>"; 
+    echo "$rfc.<br>"; 
+    echo "$postal.<br>";
+
+?>
